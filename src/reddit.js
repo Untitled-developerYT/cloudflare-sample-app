04692a471a1a5c7c30b9cc5e1cf1dba1ceabf2c0
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2026-04-26 13:05:37 +0100

    Add 2.5 second timeout to Reddit API fetch

--- src/reddit.js
+++ src/reddit.js
@@ -5,39 +5,47 @@
  * @returns The url of an image or video which is cute.
  */
 export async function getCuteUrl() {
-  const response = await fetch(redditUrl, {
-    headers: {
-      'User-Agent': 'justinbeckwith:awwbot:v1.0.0 (by /u/justinblat)',
-    },
-  });
-  if (!response.ok) {
-    let errorText = `Error fetching ${response.url}: ${response.status} ${response.statusText}`;
-    try {
-      const error = await response.text();
-      if (error) {
-        errorText = `${errorText} \n\n ${error}`;
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 2500); // 2.5 second timeout
+  
+  try {
+    const response = await fetch(redditUrl, {
+      signal: controller.signal,
+      headers: {
+        'User-Agent': 'justinbeckwith:awwbot:v1.0.0 (by /u/justinblat)',
+      },
+    });
+    if (!response.ok) {
+      let errorText = `Error fetching ${response.url}: ${response.status} ${response.statusText}`;
+      try {
+        const error = await response.text();
+        if (error) {
+          errorText = `${errorText} \n\n ${error}`;
+        }
+      } catch {
+        // ignore
       }
-    } catch {
-      // ignore
+      throw new Error(errorText);
     }
-    throw new Error(errorText);
+    const data = await response.json();
+    const posts = data.data.children
+      .map((post) => {
+        if (post.is_gallery) {
+          return '';
+        }
+        return (
+          post.data?.media?.reddit_video?.fallback_url ||
+          post.data?.secure_media?.reddit_video?.fallback_url ||
+          post.data?.url
+        );
+      })
+      .filter((post) => !!post);
+    const randomIndex = Math.floor(Math.random() * posts.length);
+    const randomPost = posts[randomIndex];
+    return randomPost;
+  } finally {
+    clearTimeout(timeout);
   }
-  const data = await response.json();
-  const posts = data.data.children
-    .map((post) => {
-      if (post.is_gallery) {
-        return '';
-      }
-      return (
-        post.data?.media?.reddit_video?.fallback_url ||
-        post.data?.secure_media?.reddit_video?.fallback_url ||
-        post.data?.url
-      );
-    })
-    .filter((post) => !!post);
-  const randomIndex = Math.floor(Math.random() * posts.length);
-  const randomPost = posts[randomIndex];
-  return randomPost;
 }
 
 export const redditUrl = 'https://www.reddit.com/r/aww/hot.json';
