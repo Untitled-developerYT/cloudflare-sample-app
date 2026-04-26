e25bbc41bf618e96cd603cd3301ec4e09dd1aa33
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2026-04-26 12:59:55 +0100

    Change response content to 'go away'

--- src/server.js
+++ src/server.js
@@ -63,7 +63,7 @@ router.post('/', async (request, env) => {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            content: cuteUrl,
+            content: "go away",
           },
         });
       }
