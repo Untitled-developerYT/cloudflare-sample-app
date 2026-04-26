9cb3f54bafa390516aa41831d6cf65c8f3ccdd5d
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2026-04-26 13:02:15 +0100

    Fix AWW_COMMAND to use cute URL and implement BOOM_COMMAND

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import {
   InteractionType,
   verifyKey,
 } from 'discord-interactions';
-import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
+import { AWW_COMMAND, BOOM_COMMAND, INVITE_COMMAND } from './commands.js';
 import { getCuteUrl } from './reddit.js';
 import { InteractionResponseFlags } from 'discord-interactions';
 
@@ -63,7 +63,15 @@ router.post('/', async (request, env) => {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            content: "go away",
+            content: cuteUrl,
+          },
+        });
+      }
+      case BOOM_COMMAND.name.toLowerCase(): {
+        return new JsonResponse({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: '💥 BOOM! 💥',
           },
         });
       }
