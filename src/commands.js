e98efb80f61c374633d7b2feb460205400490d2f
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2026-04-26 12:56:48 +0100

    Add BOOM_COMMAND for explosion action

--- src/commands.js
+++ src/commands.js
@@ -7,7 +7,10 @@ export const AWW_COMMAND = {
   name: 'awwww',
   description: 'Drop some cuteness on this channel.',
 };
-
+export const BOOM_COMMAND = {
+  name: 'explosion',
+  description: 'Drop some bombs on this channel.',
+};
 export const INVITE_COMMAND = {
   name: 'invite',
   description: 'Get an invite link to add the bot to your server',
